b112ec8bdf10ab162aadb2fb157d9afe83940635
David Corujo <dcorujo@IJC20585.carrerasresearch.org>
2023-09-07 12:31:21 +0200

    Script

--- permtest_hpc.R
+++ permtest_hpc.R
@@ -9,23 +9,33 @@ load("./regionSets/ENCODE_sets.RData")
 
 # Run permtest
 
+permResAll <- crosswisePermTest(Alist = allENCODE,
+                                   Blist = allENCODE,
+                                   genome = "hg38",
+                                   evFUN = "numOverlaps",
+                                   ranFUN = "circularRandomizeRegions",
+                                   count.once = TRUE,
+                                   ntimes = 5000,
+                                   mc.cores = 20)
+
+
 permResTF <- crosswisePermTest(Alist = TF,
                              genome = "hg38",
                              evFUN = "numOverlaps",
-                             ranFUN = "resampleGenome",
+                             ranFUN = "circularRandomizeRegions",
                              count.once = TRUE,
-                             ntimes = 1000,
+                             ntimes = 5000,
                              mc.cores = 20)
 
 permResPOLR2A <- crosswisePermTest(Alist = POLR2A,
                                Blist = HIST,
                                genome = "hg38",
                                evFUN = "numOverlaps",
-                               ranFUN = "resampleGenome",
+                               ranFUN = "circularRandomizeRegions",
                                count.once = TRUE,
-                               ntimes = 1000,
+                               ntimes = 5000,
                                mc.cores = 20)
 
 # Save results
 dir.create("hpcResults", showWarnings = FALSE)
-save(permResTF, permResPOLR2A, file = "./hpcResults/permResENCODE.RData")
+save(permResTF, permResPOLR2A, file = "./hpcResults/permResENCODE_circular.RData")
