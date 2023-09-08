f84d989a1c2fd85f765f8d6fa6729862d72edef0
David Corujo <dcorujog@gmail.com>
2023-09-08 16:17:15 +0200

    Update analysis approach

--- launch_rscript.sh
+++ launch_rscript.sh
@@ -2,11 +2,11 @@
 #SBATCH -J regioner
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=60
 #SBATCH --mem-per-cpu=3G
 #SBATCH -o regioner_%j.out
 #SBATCH -e regioner_%j.err
-#SBATCH --time=1-00:00:00
+#SBATCH --time=3-00:00:00
 
 module load R/4.2.2-foss-2021b
 
--- permtest_hpc.R
+++ permtest_hpc.R
@@ -7,35 +7,19 @@ library(regioneReloaded)
 # Load region sets
 load("./regionSets/ENCODE_sets.RData")
 
+cores <- 60
+
 # Run permtest
 
 permResALL <- crosswisePermTest(Alist = allENCODE,
                                    Blist = allENCODE,
                                    genome = "hg38",
                                    evFUN = "numOverlaps",
-                                   ranFUN = "circularRandomizeRegions",
+                                   ranFUN = "resampleGenome",
                                    count.once = TRUE,
                                    ntimes = 5000,
-                                   mc.cores = 20)
-
-
-permResTF <- crosswisePermTest(Alist = TF,
-                             genome = "hg38",
-                             evFUN = "numOverlaps",
-                             ranFUN = "circularRandomizeRegions",
-                             count.once = TRUE,
-                             ntimes = 5000,
-                             mc.cores = 20)
-
-permResHIST <- crosswisePermTest(Alist = c(POLR2A, TF),
-                               Blist = HIST,
-                               genome = "hg38",
-                               evFUN = "numOverlaps",
-                               ranFUN = "circularRandomizeRegions",
-                               count.once = TRUE,
-                               ntimes = 5000,
-                               mc.cores = 20)
+                                   mc.cores = cores)
 
 # Save results
 dir.create("hpcResults", showWarnings = FALSE)
-save(permResALL, permResTF, permResHIST, file = "./hpcResults/permResENCODE_circular.RData")
+save(permResALL, permResTF, permResHIST, file = "./hpcResults/permResENCODE.RData")
